b02a628f14387f1168c6141dda20d2cd7932e05e
Shreyas-Sadavarte <55952265+Shreyas-Sadavarte@users.noreply.github.com>
2019-10-19 17:07:13 +0530

    Add files via upload

---
+++ points.sql
@@ -0,0 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 19, 2019 at 01:36 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `curiouswrangler`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `points`
+--
+
+CREATE TABLE `points` (
+  `Username` varchar(20) NOT NULL,
+  `total_points` int(3) NOT NULL DEFAULT 0,
+  `cnt1` int(2) NOT NULL DEFAULT 0,
+  `cnt2` int(2) NOT NULL DEFAULT 0,
+  `cnt3` int(2) NOT NULL DEFAULT 0,
+  `cnt4` int(2) NOT NULL DEFAULT 0,
+  `cnt5` int(2) NOT NULL DEFAULT 0,
+  `cnt6` int(2) NOT NULL DEFAULT 0,
+  `cnt7` int(2) NOT NULL DEFAULT 0,
+  `cnt8` int(2) NOT NULL DEFAULT 0,
+  `cnt9` int(2) NOT NULL DEFAULT 0,
+  `cnt10` int(2) NOT NULL DEFAULT 0,
+  `cnt11` int(2) NOT NULL DEFAULT 0,
+  `cnt12` int(2) NOT NULL DEFAULT 0,
+  `cnt13` int(2) NOT NULL DEFAULT 0,
+  `cnt14` int(2) NOT NULL DEFAULT 0,
+  `cnt15` int(2) NOT NULL DEFAULT 0,
+  `cnt16` int(2) NOT NULL DEFAULT 0,
+  `cnt17` int(2) NOT NULL DEFAULT 0,
+  `cnt18` int(2) NOT NULL DEFAULT 0,
+  `cnt19` int(2) NOT NULL DEFAULT 0,
+  `cnt20` int(2) NOT NULL DEFAULT 0,
+  `cnt21` int(2) NOT NULL DEFAULT 0,
+  `cnt22` int(2) NOT NULL DEFAULT 0,
+  `cnt23` int(2) NOT NULL DEFAULT 0,
+  `cnt24` int(2) NOT NULL DEFAULT 0,
+  `cnt25` int(2) NOT NULL DEFAULT 0,
+  `cnt26` int(2) NOT NULL DEFAULT 0,
+  `cnt27` int(2) NOT NULL DEFAULT 0,
+  `cnt28` int(2) NOT NULL DEFAULT 0,
+  `cnt29` int(2) NOT NULL DEFAULT 0,
+  `cnt30` int(2) NOT NULL DEFAULT 0,
+  `Current_time1` time DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `points`
+--
+
+INSERT INTO `points` (`Username`, `total_points`, `cnt1`, `cnt2`, `cnt3`, `cnt4`, `cnt5`, `cnt6`, `cnt7`, `cnt8`, `cnt9`, `cnt10`, `cnt11`, `cnt12`, `cnt13`, `cnt14`, `cnt15`, `cnt16`, `cnt17`, `cnt18`, `cnt19`, `cnt20`, `cnt21`, `cnt22`, `cnt23`, `cnt24`, `cnt25`, `cnt26`, `cnt27`, `cnt28`, `cnt29`, `cnt30`, `Current_time1`) VALUES
+('Shreyas', 60, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '13:24:44'),
+('Dhanashree', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '16:48:26'),
+('Chetna', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL),
+('Wrangler1', 120, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '14:10:28'),
+('Wrangler2', 150, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '13:46:24'),
+('Wrangler3', 130, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '14:04:46'),
+('Wrangler4', 140, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '14:02:49'),
+('Wrangler5', 230, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '14:02:32'),
+('Wrangler6', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL),
+('Wrangler7', 180, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '14:07:12'),
+('Wrangler8', 310, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '14:10:11'),
+('Wrangler9', 290, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '13:49:32'),
+('Wrangler10', 170, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '14:08:00'),
+('Wrangler11', 90, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '14:00:23'),
+('Wrangler12', 110, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '13:45:14'),
+('Wrangler13', 190, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '14:04:16'),
+('Wrangler14', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL),
+('Wrangler15', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL),
+('Wrangler16', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL),
+('Wrangler17', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL),
+('Wrangler18', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL),
+('Wrangler19', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL),
+('Wrangler20', 90, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '14:08:55'),
+('', 30, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '13:24:04'),
+('Wrangler21', 140, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '14:06:50'),
+('Wrangler22', 140, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '13:33:44'),
+('Wrangler23', 30, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '13:48:08'),
+('Wrangler24', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL),
+('Wrangler25', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL),
+('Wrangler26', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL),
+('Wrangler27', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL),
+('Wrangler28', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL),
+('Wrangler29', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL),
+('Wrangler30', 140, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '14:06:15');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
